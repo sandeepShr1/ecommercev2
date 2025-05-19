b19b7697912d6c42808fd0e0d9b152d4625e3e86
sandeepshrestha2056 <sandeepshrestha2056@gmail.com>
2025-05-19 12:35:11 +0545

    added mobile-responsive header

--- script/index.js
+++ script/index.js
@@ -5,8 +5,10 @@ const menu = document.querySelector(".menus");
 const search = document.querySelector(".search");
 const utilitySection = document.querySelector(".utility-section");
 const categoriesList = document.querySelector(".categories-list");
+const navbar = document.querySelector(".navbar");
 
 burgerButton.addEventListener("click", () => {
+  navbar.classList.add("show");
   menu.classList.add("show");
   search.classList.add("show");
   utilitySection.classList.add("show");
@@ -16,6 +18,7 @@ burgerButton.addEventListener("click", () => {
 });
 
 closeButton.addEventListener("click", () => {
+  navbar.classList.remove("show");
   menu.classList.remove("show");
   search.classList.remove("show");
   utilitySection.classList.remove("show");
